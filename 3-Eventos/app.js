df438a17bc703cc6e163825a07bbde3874b7cab2
Johan Polo <johan.s310806@gmail.com>
2025-04-24 20:19:12 -0500

    Realizado de validaciones de campos en funcion validar

--- 3-Eventos/app.js
+++ 3-Eventos/app.js
@@ -11,18 +11,44 @@ const constrasena = document.querySelector('[name="constrasena"]');
 // Funciones
 const validar = (event) => {
   event.preventDefault();
-  if (!nombre.validar) {
-    alert("Debe ingresar el nombre");
+  if (!nombre.value) {
+    alert("Debe ingresar un nombre");
     nombre.focus();
+    return;
   } 
+  if(!apellido.value){
+    alert("Debe ingresar un apellido");
+    apellido.focus();
+    return;
+  }
+  if(!telefono.value){
+    alert("Debe ingresar un número telefono");
+    telefono.focus();
+    return;
+  }
+  if(!documento.value){
+    alert("Debe ingresar un número documento");
+    documento.focus();
+    return;
+  }
+  if(!usuario.value){
+    alert("Debe ingresar un nombre de usuario");
+    usuario.focus();
+    return;
+  }
+  if(!constrasena.value){
+    alert("Debe ingresar una contraseña");
+    constrasena.focus();
+    return;
+  }
 }
 // Eventos
 formulario.addEventListener("submit", validar);
 
 const teclasEspeciales = ["Backspace", "Delete", "Tab", "Enter", "Home", "End", "Shift"];
 const regexLetras = /^[a-záéíóú ]$/i;
-const regexNumeros = /^[1-9]$/;
-const regexCaracteres = /^[a-záéíóú1-9\-._&# ]$/;
+const regexNumeros = /^[0-9]$/;
+const regexCaracteres = /^[a-záéíóú0-9\-._&# ]$/i;
 
 nombre.addEventListener("keydown", (event) => {
   if(!regexLetras.test(event.key) && !teclasEspeciales.includes(event.key)) event.preventDefault();
